17b1e13feda0f816a99b4519e0530befdddc481d
Deep Patel <deeppatel074@gmail.com>
2022-12-20 13:00:59 -0500

    Added Redis Uri

--- team-up-server/src/config/redisClient.js
+++ team-up-server/src/config/redisClient.js
@@ -1,6 +1,6 @@
 import * as redis from "redis";
 const RedisClient = redis.createClient({
-  url: "redis://default:UalgWaPtqzDLjGLcxA6rASDS4TImDCXo@redis-15932.c11.us-east-1-2.ec2.cloud.redislabs.com:15932",
+  url: process.env.REDIS_URI,
 });
 RedisClient.connect().then(() => {});
 
